eb4da7f7f40d17350bb049ed815ba524a55e7ded
Alexander Drach <alexander-drach@mail.ru>
2020-05-15 17:16:06 +0600

    добавлены изображения для ретины 2х, 3х

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let humb = document.querySelector('.checkbox');
 let nav = document.querySelector('.header-page');
 let logo = document.querySelector('.logo picture source');
 
+let menuLinks = document.querySelectorAll('.nav a');
+
 humb.onclick = function () {
     nav.classList.toggle('is-open');
     if (logo.getAttribute('srcset') === 'svg/logo-mobile-menu.svg') {
@@ -12,3 +14,14 @@ humb.onclick = function () {
         logo.setAttribute('srcset', 'svg/logo-mobile-menu.svg');
     }
 };
+
+for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].onclick = function () {
+        nav.classList.remove('is-open');
+        if (logo.getAttribute('srcset') === 'svg/logo-mobile-menu.svg') {
+            logo.setAttribute('srcset', 'svg/logo-mobile.svg');
+        }else {
+            logo.setAttribute('srcset', 'svg/logo-mobile-menu.svg');
+        }
+    }
+}
